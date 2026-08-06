0a069a620b9b8536dcb3f09fce2dbcd79747a5bc
Elakiya Govindaraj <elakiyagovindaraj6@gmail.com>
2026-08-06 17:32:45 +0530

    add: spaces added

--- Backend/app/auth.py
+++ Backend/app/auth.py
@@ -71,6 +71,10 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from app.firebase_config import verify_firebase_token
 
+
+
+
+
 # Security dependency for extracting and verifying Firebase Bearer Token from HTTP Authorization Header
 security = HTTPBearer(auto_error=False)
 
